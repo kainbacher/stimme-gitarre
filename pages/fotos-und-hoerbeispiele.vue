49d1f86db295db0ffc93d30eded5a8c23f9754ea
kainbacher <rka@beautyparlour.at>
2023-01-05 09:09:54 +0100

    add youtube video

--- pages/fotos-und-hoerbeispiele.vue
+++ pages/fotos-und-hoerbeispiele.vue
@@ -1,25 +1,26 @@
 <script>
 export default {
   head: {
-    title: 'Fotos und Hörbeispiele - Melanie Schuchter - Stimme Gitarre',
+    title: "Fotos und Hörbeispiele - Melanie Schuchter - Stimme Gitarre",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'Lassen Sie uns gemeinsam ihre Hochzeit unvergesslich machen, ihre Agape, den Sektempfang, ein gemütliches Dinner zu etwas ganz Besonderen gestalten oder auch den Vortragsabend musikalisch umranden.'
-      }
+        hid: "description",
+        name: "description",
+        content:
+          "Lassen Sie uns gemeinsam ihre Hochzeit unvergesslich machen, ihre Agape, den Sektempfang, ein gemütliches Dinner zu etwas ganz Besonderen gestalten oder auch den Vortragsabend musikalisch umranden.",
+      },
     ],
-  }
-}
+  },
+};
 </script>
 
 <template>
   <main>
     <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto flex flex-wrap">
-        <div class="flex w-full mb-20 flex-wrap">
+      <div class="container px-5 pt-24 pb-4 mx-auto flex flex-wrap">
+        <div class="flex w-full py-20 flex-wrap">
           <h1
             class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4"
           >
@@ -79,6 +80,24 @@ export default {
                 src="/images/melanie-06.jpg"
               />
             </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container px-5 pb-24 mx-auto flex flex-wrap">
+        <div class="flex w-full mb-20 flex-wrap">
+          <div
+            class="relative h-0 overflow-hidden max-w-full w-full"
+            style="padding-bottom: 56.25%"
+          >
+            <iframe
+              src="https://www.youtube.com/embed/bvYSRDIN5rU"
+              frameborder="0"
+              allowfullscreen
+              class="absolute top-0 left-0 w-full h-full"
+            ></iframe>
           </div>
         </div>
       </div>
